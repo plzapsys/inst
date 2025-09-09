086c5909bf52d8927ccb652384c2606f9d52126d
plzapsys <116282861+plzapsys@users.noreply.github.com>
2025-09-09 15:47:47 +0300

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -59,7 +59,7 @@ usermod -aG docker zabbix
 
 # set IP on Zabbix server
 sed -i "s/Server=127.0.0.1/Server=127.0.0.1,192.168.53.47/g" /etc/zabbix/zabbix_agent2.conf
-sed -i "s/ServerActive=127.0.0.1/ServerActive,192.168.53.47/g" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=127.0.0.1,192.168.53.47/g" /etc/zabbix/zabbix_agent2.conf
 
 # set Hostname 
 ### Option: Hostname
@@ -68,8 +68,8 @@ sed -i "s/ServerActive=127.0.0.1/ServerActive,192.168.53.47/g" /etc/zabbix/zabbi
 #	Value is acquired from HostnameItem if undefined.
 sed -i "s/Hostname=Zabbix server/Hostname=HAProxyP43/g" /etc/zabbix/zabbix_agent2.conf
 
-sed -i "s/# Plugins.Docker.Endpoint=unix:///var/run/docker.sock/Plugins.Docker.Endpoint=unix:///var/run/docker.sock/g" /etc/zabbix/zabbix_agent2.d/plugins.d/docker.conf
-
+sed -i "s|# Plugins.Docker.Endpoint=unix:///var/run/docker.sock|Plugins.Docker.Endpoint=unix:///var/run/docker.sock|g" /etc/zabbix/zabbix_agent2.d/plugins.d/docker.conf
+systemctl restart zabbix-agent2
 tail -f /var/log/zabbix/zabbix_agent2.log
 
 
