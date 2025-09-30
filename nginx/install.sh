54452a434a0cdf6441bec11d5497e93f85275d3e
plzapsys <116282861+plzapsys@users.noreply.github.com>
2025-09-30 09:39:43 +0300

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -13,10 +13,25 @@ openssl dhparam -out /etc/pki/tls/certs/dhparam.pem 2048
 chown -R nginx:nginx /etc/pki/tls/private/nginx.key
 chmod 600 /etc/pki/tls/private/nginx.key
 
-
 chown nginx:nginx /usr/share/nginx/html -R
-mkdir /mnt/sources
-chown nginx:nginx /mnt/sources -R
-
 # test nginx
 # http://IP-HOST
+
+# -------- create strukture site-01 ------------------
+mkdir /mnt/sources
+mkdir /mnt/sources/site-01
+mkdir /mnt/sources/site-01/log
+mkdir /mnt/sources/site-01/www
+
+echo "<?php phpinfo(); ?>" > /mnt/sources/site-01/www/info.php
+echo "<?php echo "site-01"; ?>" > /mnt/sources/site-01/www/index.php
+echo "<p> site-01 </p>" > /mnt/sources/site-01/www/index.html
+
+touch /mnt/sources/site-01/log/error.log
+touch /mnt/sources/site-01/log/xdebug.log
+
+chown nginx:nginx /mnt/sources -R
+
+# http://IP-HOST:8001
+# https://IP-HOST:44301
+
