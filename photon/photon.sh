529c43842d55d171fe123157b66ef5dc42727170
plzapsys <116282861+plzapsys@users.noreply.github.com>
2025-11-26 15:50:15 +0200

    Update photon.sh

--- photon/photon.sh
+++ photon/photon.sh
@@ -7,10 +7,6 @@ sed -i "s/PermitRootLogin no/PermitRootLogin yes/g" /etc/ssh/sshd_config
 # systemctl restart sshd
 sed -i 's/WAIT_OPTIONS="--wait 1 --wait-interval 20000"/WAIT_OPTIONS="--wait 1"/g' /etc/systemd/scripts/iptables
 # sed -i "s/DHCP=yes/DHCP=no/g" /etc/systemd/network/99-dhcp-en.network
-echo "" >> /etc/systemd/network/50-static-en.network
-echo "" >> /etc/systemd/network/50-static-en.network
-echo "[DHCP]" >> /etc/systemd/network/50-static-en.network
-echo "UseDNS=false" >> /etc/systemd/network/50-static-en.network
 
 # tdnf check-update
 # tdnf clean all
