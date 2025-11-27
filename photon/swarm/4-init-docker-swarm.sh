e6867d1cdfd502765e8fd31f040e7c05eacae3c2
plzapsys <116282861+plzapsys@users.noreply.github.com>
2025-11-27 15:58:35 +0200

    Update 4-init-docker-swarm.sh

--- photon/swarm/4-init-docker-swarm.sh
+++ photon/swarm/4-init-docker-swarm.sh
@@ -1,99 +1,54 @@
 
-# eth0 - 192.168.33.x - external 
-# eth1 - 192.168.30.x - only docker swarm
-# -------------------------------------------------------------------------------------------------
-# Example
-
+ 
+# -------------------- dsw2 cluster ----------------------------------------------------------------------
 # node1
 docker swarm init \
-  --data-path-addr 192.168.30.21 \
-  --advertise-addr 192.168.30.21
+  --data-path-addr 192.168.30.41 \
+  --advertise-addr 192.168.30.41
 
+docker network create --driver=overlay --attachable obs-net 
+docker network create --driver=overlay --attachable proxy
+  
 # for manager 
 docker swarm join-token manager
-
-export SWARM_TOKEN="......"
 # node2
+export SWARM_TOKEN="......."
 docker swarm join \
   --token $SWARM_TOKEN  \
-  --data-path-addr 192.168.30.22 \
-  --advertise-addr 192.168.30.22 \
-  192.168.30.21:2377
- 
+  --data-path-addr 192.168.40.42 \
+  --advertise-addr 192.168.30.42 \
+  192.168.30.41:2377
 # node3
+export SWARM_TOKEN="......."
 docker swarm join \
   --token $SWARM_TOKEN  \
-  --data-path-addr 192.168.30.23 \
-  --advertise-addr 192.168.30.23 \
-  192.168.30.21:2377
-  
+  --data-path-addr 192.168.30.43 \
+  --advertise-addr 192.168.30.43 \
+  192.168.30.41:2377
 # ---------------------------------------------------------------------------------------------------
 # for worker 
 docker swarm join-token worker
-
-export SWARM_TOKEN="......"
 # node4
+export SWARM_TOKEN="........"
 docker swarm join \
   --token $SWARM_TOKEN \
-  --data-path-addr 192.168.30.24 \
-  --advertise-addr 192.168.30.24 \
-  192.168.30.21:2377
-
+  --data-path-addr 192.168.30.44 \
+  --advertise-addr 192.168.30.44 \
+  192.168.30.41:2377
 # node5
+export SWARM_TOKEN="......"
 docker swarm join \
   --token $SWARM_TOKEN\
-  --data-path-addr 192.168.30.25 \
-  --advertise-addr 192.168.30.25 \
-  192.168.30.21:2377
+  --data-path-addr 192.168.30.45 \
+  --advertise-addr 192.168.30.45 \
+  192.168.30.41:2377
 # node6
+export SWARM_TOKEN="......"
 docker swarm join \
   --token $SWARM_TOKEN \
-  --data-path-addr 192.168.30.26 \
-  --advertise-addr 192.168.30.26 \
-  192.168.30.21:2377
+  --data-path-addr 192.168.30.46 \
+  --advertise-addr 192.168.30.46 \
+  192.168.30.41:2377
 # ----------------------------------------------------------------------------------------------
 
-# check cluster 
-docker node ls
 
-# cerate networks 
-docker network create --driver=overlay --attachable obs-net 
-docker network create --driver=overlay --attachable proxy 
-docker network create --driver=overlay --attachable redisnet
-# check network 
-docker network inspect obs-net
-# docker service ls
-# docker stack deploy -d=true -c {compose-file} {name}
-# docker service update --force {service_name}
-# docker service rm {service}
-
-# -----------------------------------------------------------
-# Init only one network eth0
-docker swarm init --advertise-addr 192.168.23.71
-# check
-docker node ls
-# get manager token 
-docker swarm join-token manager
-# manager node 
-docker swarm join --token ...... 192.168.23.71:2377
-# get token for worker on manager-1 node
-docker swarm join-token worker
-# work node
-docker swarm join --token ..... 192.168.23.71:2377
-
-
-# reset docker swarm 
-docker stack rm <name-stack>
-
-# on all nodes
-docker swarm leave --force
-
-sudo systemctl stop docker
-sudo rm -rf /var/lib/docker/network/files/
-sudo rm -rf /var/lib/docker/swarm/state.json
-sudo rm -rf /var/lib/docker/swarm/certificates/
-sudo systemctl start docker
-docker ps -aq | xargs -r docker rm -f
-docker volume prune -f
-
-# ----------------------------------------------------------------------------------
